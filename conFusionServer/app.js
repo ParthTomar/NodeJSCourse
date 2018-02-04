c5e65ac2b4f774f5ee1d683bf727e41968811a10
parth.tomar@talentica.com <parth.tomar@talentica.com>
2018-02-04 22:32:28 +0530

    Express Rest API with MongoDB

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -11,6 +11,22 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const Dishes = require('./models/dishes');
+const url = "mongodb://localhost:27017/dishes";
+const connect = mongoose.connect(url,{
+  useMongoClient: true
+});
+
+connect.then((db)=>{
+  console.log('Connected correctly to the server');
+})
+.catch((err)=>{
+  console.log(err);
+});
+
 var app = express();
 
 // view engine setup
