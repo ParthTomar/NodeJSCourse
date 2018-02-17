eb7ce361809c99b22c051dd2b4245bf0321a1874
parth.tomar@talentica.com <parth.tomar@talentica.com>
2018-02-17 17:36:22 +0530

    Passport Facebook Oauth

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     "secretKey": "12345-67890-09876-54321",
-    "mongoUrl": "mongodb://localhost:27017/conFusion"
+    "mongoUrl": "mongodb://localhost:27017/conFusion",
+    "facebook": {
+        clientId: '1452738044977532',
+        clientSecret: '974934f4514533e63c151b0ed16d2071' 
+    }
 };
--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -16,7 +16,7 @@ router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.veri
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
       res.json(users);
-    }, (err)=>{next(err)});
+    }, (err) => { next(err) });
 });
 
 router.post('/signup', cors.corsWithOptions, (req, res, next) => {
@@ -70,5 +70,14 @@ router.get('/logout', cors.corsWithOptions, (req, res) => {
     return next(err);
   }
 });
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    var token = authenticate.getToken({ _id: req.user._id });
+    res.status = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ success: true, token: token, status: 'You are successfully logged in!' });
+  }
+})
 
 module.exports = router;
